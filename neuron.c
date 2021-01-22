ea43dda28cabcb439ca5316e1f6a9e27e65cc66f
andkalforSGU <57719545+andkalforSGU@users.noreply.github.com>
2021-01-23 02:53:00 +0400

    Add files via upload

--- neuron.c
+++ neuron.c
@@ -10,10 +10,9 @@ typedef struct {
 } neuron;
 
 typedef struct {
-    int a;
-    int b;
-    neuron* layer;
+    int layers_count;
     neuron** layers;
+    neuron out_neu;
 } net;
 
 float sigmoid(float x);
@@ -26,26 +25,37 @@ neuron createNeu(
     float (*adderFunc)(float*, float*, int)
 );
 
-net createNet(neuron N, int a, int b);
+net createNet(
+    neuron (*createNeu)(int size, float (*normalizeFunc)(float), 
+    float (*adderFunc)(float*, float*, int)),
+    int layers_count,
+    int feacheres_count,
+    float (*normilizeFunc)(float),
+    float (*adderFunc)(float*, float*, int)
+);
 
 //----------------POINTERS_OF_FUNCTIONS---------------
 
 float (*normilizeFunc)(float);
 float (*adderFunc)(float*, float*, int);
-
+neuron (*createNeuPoi)(
+    int size, float (*normalizeFunc)(float), 
+    float (*adderFunc)(float*, float*, int)
+);
 //----------------------------------------------------
 
 int main(int argc, char* argv[]) {
     int size = 3;
-    float features[] = {1.0, 1.0, 1.0};
+    float features[] = {0.0, 0.0, 0.0};
     
     normilizeFunc = &sigmoid;
     adderFunc = &NET;
+    createNeuPoi = &createNeu;
 
-    neuron N = createNeu(size, normilizeFunc, NET);
-    net Net = createNet(N, 3, 3);
+    net N = createNet(createNeuPoi, 3, size, normilizeFunc, adderFunc);
 
-    printf("%f\n", Net.layers[0][0].weight[0]);
+    float out = useNet(N, features);
+    printf("%f\n", out);
 
     return 0;
 }
@@ -65,7 +75,6 @@ float NET(float* weigth, float* feaches, int size) {
         net += weigth[i] * feaches[i];
     }
 
-    printf("%f\n", net);
     return net;
 }
 
@@ -73,6 +82,7 @@ neuron createNeu(
     int size, float (*normalizeFunc)(float), 
     float (*adderFunc)(float*, float*, int)
 ) {
+    
     float* weight = (float*)malloc(size*sizeof(float));
     for (int i = 0; i < size; i++) {
         weight[i] = ((float)(rand()%10))/10;
@@ -82,27 +92,60 @@ neuron createNeu(
     return N;
 }
 
+net createNet(
+    neuron (*createNeuPoi)(int size, float (*normalizeFunc)(float), 
+    float (*adderFunc)(float*, float*, int)),
+    int layers_count,
+    int feacheres_count,
+    float (*normilizeFunc)(float),
+    float (*adderFunc)(float*, float*, int)
+    ) {
+    neuron** layers = (neuron**)malloc(layers_count*sizeof(neuron*));
+    for (int i = 0; i < layers_count; i++) {
+        layers[i] = (neuron*)malloc(feacheres_count*sizeof(neuron));
+    }
+
+    for (int i = 0; i < layers_count; i++) {
+        for (int j = 0; j < feacheres_count; j++) {
+            layers[j][i] = createNeu(feacheres_count, normilizeFunc, adderFunc);
+        }
+    }
+
+    neuron out_neu = createNeu(feacheres_count, normilizeFunc, adderFunc);
+
+    net Net = {layers_count, layers, out_neu};
+    return Net;
+}
+
 float useNeu(neuron N, float* features) {
     float net = N.adderFunc(N.weight, features, N.size);
-    printf("%f\n", net);
 
     return(N.normalizeFunc(net));
 }
 
-net createNet(neuron N, int a, int b) {
-    neuron* layer = (neuron*)malloc(a*sizeof(neuron));
-    neuron** layers = (neuron**)malloc(b*sizeof(neuron*));
-    for (int i = 0; i < a; i++) {
-        layer[i] = N;
-    }
-    for (int i = 0; i < b; i++) {
-        layers[i] = layer;
-    }
-
-    net Net = {a, b, layer, layers};
-    return Net;
-}
-
 float useNet(net Net, float* features) {
-    
+    int marker;
+    float* out_a = (float*)malloc(Net.layers[0][0].size*sizeof(float));
+    float* out_b = (float*)malloc(Net.layers[0][0].size*sizeof(float));
+    for (int i = 0; i < Net.layers[0][0].size; i++) {
+        out_a[i] = useNeu(Net.layers[i][0], features);
+    }
+
+    for (int i = 1; i < Net.layers_count; i++) {
+        for (int j = 0; j < Net.layers[0][0].size; j++) {
+            if (i % 2 == 1) {
+                out_b[j] = useNeu(Net.layers[j][i], out_a);
+                marker = 0;
+            } else if (i % 2 == 0) {
+                out_a[j] = useNeu(Net.layers[j][i], out_b);
+                marker = 1;
+            }
+        }
+    }
+
+    if (marker == 0) {
+        return(useNeu(Net.out_neu, out_b));
+    } else if (marker == 1) {
+        return(useNeu(Net.out_neu, out_a));
+    } else return 1.1;
 }
